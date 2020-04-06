894097cea814a8af6ce4cb514df46d8f9776d1df
Lucas Henriques <lucashenriques@localhost.localdomain>
2020-04-06 10:17:46 -0300

    Implementação da função de ações específicas

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify
-from fundamentus import get_data
+from fundamentus import get_data, get_specific_data
 from datetime import datetime
 
 app = Flask(__name__)
@@ -22,5 +22,11 @@ def json_api():
         lista, dia = dict(get_data()), datetime.strftime(datetime.today(), '%d')
         lista = {outer_k: {inner_k: float(inner_v) for inner_k, inner_v in outer_v.items()} for outer_k, outer_v in lista.items()}
         return jsonify(lista)
+
+@app.route("/stocks/<stock>")
+def getStock(stock):
+    lista = get_specific_data(stock)
+
+    return jsonify(lista) 
 
 app.run(debug=True)
